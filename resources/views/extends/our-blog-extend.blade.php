9c379082e27e1d0a9c7dfb94fd8b07e4a5c8ff1d
Vyrayuth Yothin <euwanfibient@yahoo.com>
2021-06-17 13:59:44 +0700

    4th Push (Full Includes)

--- resources/views/extends/our-blog-extend.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.blog')
-
-@section('content')
-    <h1> Our Blog </h1>
-@endsection
--- resources/views/extends/watch-extend.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.ani-watch')
-
-@section('content')
-    <h1> Anime Watching </h1>
-@endsection
